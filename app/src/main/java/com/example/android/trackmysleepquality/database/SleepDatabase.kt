d5d651f3343db8eb80f6a4b5d48125055feecbec
Rosy Sunuwar <>
2021-08-20 17:48:06 -0500

    create database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,16 +16,49 @@
 
 package com.example.android.trackmysleepquality.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 // TODO (01) Create an abstract class that extends RoomDatabase.
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
 
-// TODO (02) Declare an abstract value of type SleepDatabaseDao.
+    // TODO (02) Declare an abstract value of type SleepDatabaseDao.
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
-// TODO (03) Declare a companion object.
+    // TODO (03) Declare a companion object.
+    companion object {
 
-// TODO (04) Declare a @Volatile INSTANCE variable.
+        // TODO (04) Declare a @Volatile INSTANCE variable.
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
 
-// TODO (05) Define a getInstance() method with a synchronized block.
+        // TODO (05) Define a getInstance() method with a synchronized block.
+        fun getInstance(context: Context): SleepDatabase {
 
-// TODO (06) Inside the synchronized block:
-// Check whether the database already exists,
-// and if it does not, use Room.databaseBuilder to create it.
+            // TODO (06) Inside the synchronized block:
+            synchronized(lock = this) {
+                var instance = INSTANCE
+
+                // Check whether the database already exists,
+                // and if it does not, use Room.databaseBuilder to create it.
+                if (instance == null) {
+
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep history database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
