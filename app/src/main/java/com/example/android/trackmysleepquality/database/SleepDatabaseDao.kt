517afc0656c2e1ea9053d4aeb98afe8cdcb47e2c
Rosy Sunuwar <>
2021-08-20 17:22:32 -0500

    add DAO and queries

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,20 +16,38 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 // TODO (01) Create annotated interface SleepDatabaseDao.
+
+@Dao
 interface SleepDatabaseDao {
 
 // TODO (02) Add annotated insert() method for inserting a single SleepNight.
+    @Insert
+    fun insert(night: SleepNight)
 
 // TODO (03) Add annotated update() method for updating a SleepNight.
+    @Update
+    fun update(night: SleepNight)
 
 // TODO (04) Add annotated get() method that gets the SleepNight by key.
+    @Query("Select * from daily_sleep_quality_table where nightId = :key")
+    fun get(key: Long): SleepNight
 
 // TODO (05) Add annotated clear() method and query.
+    @Query("Delete from daily_sleep_quality_table")
+    fun clear()
 
 // TODO (06) Add annotated getAllNights() method and query.
+    @Query("Select * from daily_sleep_quality_table order by nightId desc")
+    fun getAllNights(): LiveData<List<SleepNight>>
 
 // TODO (07) Add annotated getTonight() method and query.
-
+    @Query("Select * from daily_sleep_quality_table order by nightId desc limit 1")
+    fun getTonight(): SleepNight?
 }
-
